4adf089ad2366cab1bb5ab8ae0ff39aabbc1eb04
Rizqy Prastya Ari Nugroho <rizqynugroho9@gmail.com>
2020-11-21 18:22:29 +0700

    BackAruga

--- index.js
+++ index.js
@@ -1,98 +1,88 @@
-const { create, Client } = require("@open-wa/wa-automate");
-const figlet = require("figlet");
-const gradient = require("gradient-string");
-const style = require("./custom/console");
-const options = require("./utils/options");
-const { color, messageLog } = require("./utils");
-const HandleMsg = require("./HandleMsg"); 
-const { cacheMessage, groupLimit, botName} = require("./bot-setting.json"); //! Bot Setting
-const {infoFeedback,infoProblem} = require('./msg/msg-temp') //! Massage Template
+const { create, Client } = require('@open-wa/wa-automate')
+const figlet = require('figlet')
+const options = require('./utils/options')
+const { color, messageLog } = require('./utils')
+const HandleMsg = require('./HandleMsg')
 
-const start = (bocilClient = new Client()) => {
-    console.log(
-        gradient.instagram(
-          figlet.textSync("RIZQY\nSTUDIO", {
-            font: "Epic",
-            horizontalLayout: "default",
-          })
-        )
-      );
-      console.log(style.dev("Made by R-Dev Studio"));
-      console.log(style.bot("Have a nice day Rizqy :)"));
-      console.log(style.bot("I'm ready for my Jobs"));
-    
+const start = (aruga = new Client()) => {
+    console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
+    console.log(color(figlet.textSync('ARUGA BOT', { font: 'Ghost', horizontalLayout: 'default' })))
+    console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
+    console.log(color('[DEV]'), color('ArugaZ', 'yellow'))
+    console.log(color('[~>>]'), color('BOT Started!', 'green'))
+
     // Mempertahankan sesi agar tetap nyala
-    bocilClient.onStateChanged((state) => {
+    aruga.onStateChanged((state) => {
         console.log(color('[~>>]', 'red'), state)
-        if (state === 'CONFLICT' || state === 'UNLAUNCHED') bocilClient.forceRefocus()
+        if (state === 'CONFLICT' || state === 'UNLAUNCHED') aruga.forceRefocus()
     })
 
     // ketika bot diinvite ke dalam group
-    bocilClient.onAddedToGroup(async (chat) => {
-	const groups = await bocilClient.getAllGroups()
+    aruga.onAddedToGroup(async (chat) => {
+	const groups = await aruga.getAllGroups()
 	// kondisi ketika batas group bot telah tercapai,ubah di file settings/setting.json
 	if (groups.length > groupLimit) {
-	await bocilClient.sendText(chat.id, `Sorry, the group on this bot is full\nMax Group is: ${groupLimit}`).then(() => {
-	      bocilClient.leaveGroup(chat.id)
-	      bocilClient.deleteChat(chat.id)
+	await aruga.sendText(chat.id, `Sorry, the group on this bot is full\nMax Group is: ${groupLimit}`).then(() => {
+	      aruga.leaveGroup(chat.id)
+	      aruga.deleteChat(chat.id)
 	  }) 
 	} else {
 	// kondisi ketika batas member group belum tercapai, ubah di file settings/setting.json
 	    if (chat.groupMetadata.participants.length < memberLimit) {
-	    await bocilClient.sendText(chat.id, `Sorry, BOT comes out if the group members do not exceed ${memberLimit} people`).then(() => {
-	      bocilClient.leaveGroup(chat.id)
-	      bocilClient.deleteChat(chat.id)
+	    await aruga.sendText(chat.id, `Sorry, BOT comes out if the group members do not exceed ${memberLimit} people`).then(() => {
+	      aruga.leaveGroup(chat.id)
+	      aruga.deleteChat(chat.id)
 	    })
 	    } else {
-        await bocilClient.simulateTyping(chat.id, true).then(async () => {
-          await bocilClient.sendText(chat.id, `Hai minna~, Im bocilClient BOT. To find out the commands on this bot type ${prefix}menu`)
+        await aruga.simulateTyping(chat.id, true).then(async () => {
+          await aruga.sendText(chat.id, `Hai minna~, Im Aruga BOT. To find out the commands on this bot type ${prefix}menu`)
         })
 	    }
 	}
     })
 
     // ketika seseorang masuk/keluar dari group
-    bocilClient.onGlobalParicipantsChanged(async (event) => {
-        const host = await bocilClient.getHostNumber() + '@c.us'
+    aruga.onGlobalParicipantsChanged(async (event) => {
+        const host = await aruga.getHostNumber() + '@c.us'
         // kondisi ketika seseorang diinvite/join group lewat link
         if (event.action === 'add' && event.who !== host) {
-            await bocilClient.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
+            await aruga.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
         }
         // kondisi ketika seseorang dikick/keluar dari group
         if (event.action === 'remove' && event.who !== host) {
-            await bocilClient.sendTextWithMentions(event.chat, `Good bye @${event.who.replace('@c.us', '')}, We'll miss you✨`)
+            await aruga.sendTextWithMentions(event.chat, `Good bye @${event.who.replace('@c.us', '')}, We'll miss you✨`)
         }
     })
 
-    bocilClient.onIncomingCall(async (callData) => {
+    aruga.onIncomingCall(async (callData) => {
         // ketika seseorang menelpon nomor bot akan mengirim pesan
-        await bocilClient.sendText(callData.peerJid, 'Maaf sedang tidak bisa menerima panggilan.\n\n-bot')
+        await aruga.sendText(callData.peerJid, 'Maaf sedang tidak bisa menerima panggilan.\n\n-bot')
         .then(async () => {
             // bot akan memblock nomor itu
-            await bocilClient.contactBlock(callData.peerJid)
+            await aruga.contactBlock(callData.peerJid)
         })
     })
 
     // ketika seseorang mengirim pesan
-    bocilClient.onMessage(async (message) => {
-        bocilClient.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
+    aruga.onMessage(async (message) => {
+        aruga.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
             .then((msg) => {
                 if (msg >= 3000) {
-                    console.log('[bocilClient]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
-                    bocilClient.cutMsgCache()
+                    console.log('[aruga]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
+                    aruga.cutMsgCache()
                 }
             })
-        HandleMsg(bocilClient, message)    
+        HandleMsg(aruga, message)    
     
     })
 	
     // Message log for analytic
-    bocilClient.onAnyMessage((anal) => { 
+    aruga.onAnyMessage((anal) => { 
         messageLog(anal.fromMe, anal.type)
     })
 }
 
 //create session
 create(options(true, start))
-    .then((bocilClient) => start(bocilClient))
+    .then((aruga) => start(aruga))
     .catch((err) => new Error(err))
